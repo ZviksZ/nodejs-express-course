19c431e224c7c04efa67443332aeb653854176d3
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2021-01-31 17:44:51 +0300

    add handlebars engine and some views

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express')
 const path = require('path')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
-app.get('/', (req, res) => {
-   res.status(200)
-
-   res.sendFile(path.join(__dirname, 'views', 'index.html'))
+const hbs = exphbs.create({
+   defaultLayout: 'main',
+   extname: 'hbs'
 })
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+   //res.status(200)
+   //res.sendFile(path.join(__dirname, 'views', 'index.hbs'))
+   res.render('index')
+})
+app.get('/courses', (req, res) => {
+   res.render('courses')
+})
+app.get('/add', (req, res) => {
+   res.render('add')
+})
+
 
 const PORT = process.env.PORT || 3000
 
