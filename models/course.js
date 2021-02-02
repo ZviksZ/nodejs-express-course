df6a238796515d20748e490626d07b2be5a7aa55
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2021-02-02 10:51:00 +0300

    update course

--- models/course.js
+++ models/course.js
@@ -38,6 +38,27 @@ class Course {
       })
    }
 
+   static async update(course) {
+      const courses = await Course.getAll()
+
+      const idx = courses.findIndex(c => c.id === course.id)
+      courses[idx] = course
+
+      return new Promise((resolve, reject) => {
+         fs.writeFile(
+            path.join(__dirname, '..', 'data', 'courses.json'),
+            JSON.stringify(courses),
+            (error) => {
+               if (error) {
+                  reject(error)
+               } else {
+                  resolve()
+               }
+            }
+         )
+      })
+   }
+
    static getAll() {
       return new Promise((resolve, reject) => {
          fs.readFile(
